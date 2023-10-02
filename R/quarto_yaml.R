01d7ffbffcf615d371ca4b890960ea0afea70d27
Galen Holt <53928785+galenholt@users.noreply.github.com>
2023-10-03 09:38:55 +1100

    change 'functions' to R

---
+++ R/quarto_yaml.R
@@ -0,0 +1,52 @@
+use_quarto_yaml <- function(yamfile = 'project', yamdir = '_yml') {
+  yamfiles <- list.files(yamdir)
+  whichyam <- grepl(yamfile, yamfiles)
+  if (sum(whichyam) > 1) {stop('too many matching yaml files')}
+  
+  file.copy(file.path(yamdir, yamfiles[whichyam]), '_quarto.yml', overwrite = TRUE)
+  
+  return(invisible())
+}
+
+make_simple_yaml <- function(proj_yaml_file = NULL,
+                             yamdir = getwd(), 
+                             simple_file = NULL) {
+  # By default, assume yaml is in working directory/_quarto.yml
+  if (is.null(proj_yaml_file)) {
+    proj_yaml_file <- file.path(yamdir, '_quarto.yml')
+  }
+  
+  proj_yaml <- yaml::read_yaml(proj_yaml_file)
+  
+  simple_yaml <- list() 
+  simple_yaml$project <- proj_yaml$project
+  # kill the type in case it's complex (e.g. website, book)
+  simple_yaml$project$type <- NULL
+  # kill render options- only rendering a single doc should allow rendering that doc and no others
+  simple_yaml$project$render <- NULL
+  
+  if (is.null(simple_file)) {
+    simple_file <- file.path(yamdir, '_quarto_simple.yml')
+  }
+  
+  yaml::write_yaml(simple_yaml, simple_file)
+  
+}
+
+make_multi_yaml <- function(yamdir = '_yml', 
+                            copy_orig = TRUE, 
+                            make_simple = TRUE,
+                            leave_orig = TRUE) {
+  
+  
+  if (!dir.exists(yamdir)) {dir.create(yamdir)}
+  
+  if (copy_orig) {
+    file.copy('_quarto.yml', file.path(yamdir, '_quarto_project.yml'))
+    if (!leave_orig) {file.remove('_quarto.yml')}
+  }
+  
+  if (make_simple) {
+    make_simple_yaml(file.path(yamdir, '_quarto_project.yml'), yamdir = yamdir)
+  }
+}
